337dcd9f9361e747521d825a55a0bd11c6b0571c
Gusarajo <15132@portalcedi.edu.mx>
2025-01-23 08:52:38 -0600

    Final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tasks: any = [];
+  NombreE = new FormControl('');
+  Nombre = new FormControl('');
+  Numero = new FormControl('');
+  ubicacion = new FormControl('');
+  FechaI = new FormControl('');
+  FechaF = new FormControl('');
 
+  constructor() { }
+  buttonclick() {
+    if (this.NombreE.value && this.Nombre.value && this.Numero.value && this.ubicacion.value && this.FechaI.value && this.FechaF.value) {
+      const newtask = {
+        nombreE: this.NombreE.value,
+        nombre: this.Nombre.value,
+        numero: this.Numero.value,
+        Ubicacion: this.ubicacion.value,
+        fechaI: this.FechaI.value,
+        fechaF: this.FechaF.value
+      };
+      this.tasks.push(newtask);
+      console.log('se agrego');
+      this.NombreE.setValue('');
+      this.Nombre.setValue('');
+      this.Numero.setValue('');
+      this.ubicacion.setValue('');
+      this.FechaI.setValue('');
+      this.FechaF.setValue('');
+    } else {
+      alert('llena los campos')
+    }
+  }
 }
